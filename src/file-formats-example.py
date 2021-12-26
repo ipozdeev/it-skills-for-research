739c382db5c3a1bd429b95c588205813dcb27f5b
ipozdeev <igor.pozdeev@uzh.ch>
2021-12-26 22:18:41 +0100

    better layout

--- src/file-formats-example.py
+++ src/file-formats-example.py
@@ -21,10 +21,16 @@ f"file size (Mb): {os.path.getsize('src/abc.zip') / 1e06}"
 %time df.to_pickle("src/abc.pkl")
 f"file size (Mb): {os.path.getsize('src/abc.pkl') / 1e06}"
 
+# to hdf
+%time df.to_hdf("src/abc.h5", key="fx/spot")
+f"file size (Mb): {os.path.getsize('src/abc.h5') / 1e06}"
+
 # to feather
 %time feather.write_feather(df, "src/abc.arrow")
 f"file size (Mb): {os.path.getsize('src/abc.arrow') / 1e06}"
 
 # read in -------------------------------------------------------------------
 %time df_csv = pd.read_csv("src/abc.zip", compression="zip")
-%time df_arrow = pd.read_feather("src/abc.arrow")
+%time df_pkl = pd.read_pickle("src/abc.pkl")
+%time df_h5 = pd.read_hdf("src/abc.h5")
+%time df_ftr = pd.read_feather("src/abc.arrow")
